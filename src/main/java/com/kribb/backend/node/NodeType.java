5befcb97849ecd3fdbeee88b61e9c85140431dda
JisuEun <zkxktmxmfhvl@gmail.com>
2025-10-12 22:29:54 +0900

    chore: Added NodeType [COMPOUND]

--- src/main/java/com/kribb/backend/node/NodeType.java
+++ src/main/java/com/kribb/backend/node/NodeType.java
@@ -1,3 +1,3 @@
 // NodeType.java
 package com.kribb.backend.node;
-public enum NodeType { PDB, SDF, SMILES, GENERIC, VISUALIZER, SECONDARY , DEEPKINOME}
+public enum NodeType { PDB, SDF, SMILES, GENERIC, VISUALIZER, SECONDARY , DEEPKINOME, COMPOUND}
